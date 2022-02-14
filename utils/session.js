82efc6aa378aa70c6b86789559ee35c5ff64151c
samkindev <samkyunyudev@gmail.com>
2022-02-14 19:59:24 +0100

    Changed the session handler

--- utils/session.js
+++ utils/session.js
@@ -2,7 +2,7 @@
 // Interaction avec la session PHP
 
 // Appel aux modules externes
-const fs = require("fs");
+var Memcached = require('memcached');
 const path = require('path');
 const phpUnserialize = require("php-unserialize");
 const { getAssujetti, getUser } = require("./helpers");
@@ -11,13 +11,9 @@ const getDatabase = require('../models/assujettiDb');
 class UserSession {
 	/**
 	 * Constructeur d'initialisation de l'objet de session utilisateur
-	 * @param {string} path Chemin racine d'accès au ficher de session
 	 */
-	constructor(path) {
-		if (!path || path === "") {
-			throw new Error("Session file path is required");
-		}
-		this.path = path;
+	constructor() {
+		this.session_prefix = "memc.sess.key.";
 		this.readSession = this.readSession.bind(this);
 		this.getUserData = this.getUserData.bind(this);
 	}
@@ -27,47 +23,62 @@ class UserSession {
 	 * @param {string} numDef Def number of the user
 	 * @returns user db name
 	 */
-	static getBdName = (numDef) => {
+	static getBdName(numDef) {
 		return "def_" + numDef.replaceAll("/", "").replaceAll("-", "").toLowerCase();
 	}
 
 	// Fonction de lecture de la session à partir du fichier tmp
 	readSession(req, res, next) {
+		const unauthMessage = "Aucune session utilisateur.";
 		// Recuperation du cookie
 		const sid = req.cookies;
 		// Verification de l'existance du cookie PHPSESSID
-		if (sid.PHPSESSID) {
-			// Lecture du fichier de session
-			//
-			// * S'il n'y a pas erreur
-			//  - Decoder les données de la session
-			//  - Création de l'attribut session sur la variable globale de la requête
-			//  - Et passer à la fonction suivante
-			//
-			// * Si eureur
-			//  - Afficher l'erreur
-			//  - Retourner la réponse de non authentification
-			fs.readFile(
-				this.path + "sess_" + sid.PHPSESSID,
-				"utf-8",
-				function (err, data) {
-					if (!err) {
-						var sd = phpUnserialize.unserializeSession(data);
+		try {
+			if (sid.PHPSESSID) {
+				// Lecture de la session utilisateur dans la memoire cache Memcache
+				//
+				// * S'il n'y a pas erreur
+				//  - Decoder la session avec phpUnserialize
+				//  - Création de l'attribut session sur la variable globale de la requête
+				//  - Et passer à la fonction suivante
+				//
+				// * Si eureur
+				//  - Renvoyer une nouvelle erreur
+				//  - Retourner la réponse de non authentification
+
+				// Connextion Memcache
+				var mem = new Memcached("127.0.0.1:11211");
+				// La session avec le prefix de memcache session
+				const memSID = this.session_prefix + sid.PHPSESSID;
+				mem.get(memSID, (err,data) => {
+					if ( err ) {
+						console.error(err);
+						throw new Error(unauthMessage);
+					} else if (data) {
+						const sd = phpUnserialize.unserializeSession(data);
 						req.session = sd;
+						console.log(sd);
 						next();
 					} else {
 						console.log(err);
-						return res.sendFile(path.join(__dirname, '../client/build/index.html'));
+						throw new Error(unauthMessage);
 					}
-				}
-			);
-		} else {
+				});
+			} else {
+				throw new Error(unauthMessage);
+			}
+		} catch (error) {
+			// Redirection page d'accueil qui renvera l'utilisateur a la page login php
 			return res.sendFile(path.join(__dirname, '../client/build/index.html'));
 		}
 	}
 
-    // Fonction de recuperation du nom de la table
-	getTabName = (catAssujetti) => {
+	/**
+	 * Recupere le nom de la table de categorie
+	 * @param {string} catAssujetti La categorie de l'assujetti
+	 * @returns la chaine des caractere qui represente la table de categorie assujetti
+	 */
+	getTabName (catAssujetti) {
 		let table = "";
 		switch (catAssujetti) {
 			case "pmc":
